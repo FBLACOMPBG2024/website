20830becd049848114597eb31ae4fea1cd09c68e
Trent <no@email.com>
2025-01-04 02:27:08 -0600

    Enough animations for tonight

--- src/components/ui/dashboard/TransactionsView.tsx
+++ src/components/ui/dashboard/TransactionsView.tsx
@@ -300,17 +300,17 @@ export default function TransactionsView() {
               <tbody>
                 {transactions.map((transaction: Transaction, index: number) => (
                   <motion.tr
-                    key={index}
-                    initial={{ opacity: 0, y: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 10 }}
-                    transition={{ duration: 0.2, delay: index * 0.05 }}
-                    className={
-                      index < transactions.length - 1
-                        ? "border-b border-backgroundGrayLight"
-                        : ""
-                    }
-                  >
+                  key={index}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }} // Adjust exit animation to slide out
+                  transition={{ duration: 0.2, delay: index * 0.05 }}
+                  className={
+                    index < transactions.length - 1
+                      ? "border-b border-backgroundGrayLight"
+                      : ""
+                  }
+                >                
                     <td className="px-4 py-2">{transaction.name}</td>
                     <td className="px-4 py-2">
                       {transaction.value.toLocaleString("en-US", {
